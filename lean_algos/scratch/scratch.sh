40492f93c420f38a19258305d3c7ca84c468c45c
vrindger <58531716+vrindger@users.noreply.github.com>
2025-03-21 21:31:48 -0600

    Update scratch.sh

--- lean_algos/scratch/scratch.sh
+++ lean_algos/scratch/scratch.sh
@@ -1,13 +1,16 @@
+cp -rf ../../base .
+BASE_DIR=./base
+
 ALGO=scratch
 lower_case_algo=$(echo $ALGO | tr '[:upper:]' '[:lower:]')
 
-bash docker_cleanup.sh
+bash $BASE_DIR/docker_cleanup.sh  # optional if u want to save each docker image/container for some weird reason
 
 docker build --build-arg CONFIG_TO_USE=$ALGO \
             --build-arg GITHUB_USERNAME=$GITHUB_USERNAME \
             --build-arg GITHUB_PAT=$GITHUB_PAT \
             -t lean_$lower_case_algo:debug \
-            -f Dockerfile_scratch .
+            -f Dockerfile_scratch .x
 
 # run built image
 docker run -it lean_$lower_case_algo:debug
